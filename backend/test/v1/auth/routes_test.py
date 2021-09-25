eeb2542afcf8a3013ec544056530372a6624b0e3
Nikolas North <nikolasn09@gmail.com>
2021-09-24 20:39:26 -0400

    add tests for missing required fields

--- backend/test/v1/auth/routes_test.py
+++ backend/test/v1/auth/routes_test.py
@@ -4,28 +4,33 @@ from audiobyte.api.errors import INVALID_JSON_REQUEST, MISSING_REQUIRED_JSON
 url = "/api/v1/auth"
 
 
-def test_index():
-  app = create_app()
-  with app.test_client() as client:
-    res = client.get(f"{url}/")
-    assert res.status_code == 200
-
-
 def test_signup_without_json_mimetype():
   app = create_app()
   with app.test_client() as client:
     res = client.post(f"{url}/signup")
     data = res.get_json()
-    err_code, err_msg = INVALID_JSON_REQUEST
+    want_code, want_msg = INVALID_JSON_REQUEST
+    assert res.status_code == want_code
+    assert data and data["error"] == want_msg
+
+
+def test_signup_without_required_username_field():
+  app = create_app()
+  with app.test_client() as client:
+    res = client.post(f"{url}/signup", json={
+      "password": "abc123",
+    })
+    data = res.get_json()
+    err_code, err_msg = MISSING_REQUIRED_JSON
     assert res.status_code == err_code
     assert data and data["error"] == err_msg
 
 
-def test_signup_without_required_field():
+def test_signup_without_required_password_field():
   app = create_app()
   with app.test_client() as client:
     res = client.post(f"{url}/signup", json={
-      "password": "abc123"
+      "username": "username",
     })
     data = res.get_json()
     err_code, err_msg = MISSING_REQUIRED_JSON
@@ -43,11 +48,23 @@ def test_signin_without_json_mimetype():
     assert data and data["error"] == err_msg
 
 
-def test_signin_without_required_field():
+def test_signin_without_required_username_field():
   app = create_app()
   with app.test_client() as client:
     res = client.post(f"{url}/signup", json={
-      "password": "abc123"
+      "password": "abc123",
+    })
+    data = res.get_json()
+    err_code, err_msg = MISSING_REQUIRED_JSON
+    assert res.status_code == err_code
+    assert data and data["error"] == err_msg
+
+
+def test_signin_without_required_password_field():
+  app = create_app()
+  with app.test_client() as client:
+    res = client.post(f"{url}/signup", json={
+      "username": "username",
     })
     data = res.get_json()
     err_code, err_msg = MISSING_REQUIRED_JSON
