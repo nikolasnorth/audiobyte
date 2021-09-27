5fec86231ceddee31faf82d432a56261838f6f91
Nikolas North <nikolasn09@gmail.com>
2021-09-26 20:47:39 -0400

    switch to nestjs

--- backend/audiobyte/api/auth/routes.py
+++
@@ -1,8 +0,0 @@
-from fastapi import APIRouter
-
-auth = APIRouter(prefix="/api/v1/auth")
-
-
-@auth.get("/signin")
-async def signin():
-  return {"msg": "hello from /auth/v1/signin"}
--- backend/main.py
+++
@@ -1,4 +0,0 @@
-from uvicorn import run
-
-if __name__ == '__main__':
-  run("audiobyte:app", host="127.0.0.1", port=8000, reload=True)
