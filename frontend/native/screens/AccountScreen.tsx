2525018bf0a63cc8657997fd4c5a3c2415db2313
Nikolas North <nikolasn09@gmail.com>
2021-11-21 12:23:26 -0500

    remove line break

--- frontend/native/screens/AccountScreen.tsx
+++ frontend/native/screens/AccountScreen.tsx
@@ -6,7 +6,6 @@ export default function AccountScreen() {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Account</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)"/>
     </View>
   );
 }
@@ -20,10 +19,5 @@ const styles = StyleSheet.create({
   title: {
     fontSize: 20,
     fontWeight: "bold",
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: "80%",
   },
 });
